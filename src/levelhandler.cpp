f2e8b7f0ac695f54b272f0365f3afe0ffe81a58d
DaPasta <484902@gmail.com>
2015-06-22 22:06:22 -0700

    Reformatted map xml data to use attributes for future use

--- src/levelhandler.cpp
+++ src/levelhandler.cpp
@@ -10,16 +10,20 @@ LevelHandler::LevelHandler(const char* path) {
 }
 
 void LevelHandler::createLevels(std::vector<Level> &levels) {
+    std::string platformName = "platform";
     if(isLoaded) {
         std::cout << "Creating levels..." << std::endl;
-        for(pugi::xml_node level = file.child("level"); level; level = level.next_sibling()) {
+        for(pugi::xml_node level = file.child("levels").first_child(); level; level = level.next_sibling()) {
             std::cout << "Level " << level.attribute("id").value() << ":" << std::endl;
             for(pugi::xml_node node = level.first_child(); node; node = node.next_sibling()) {
                 std::cout << "\t" << node.name() << " : " << node.value() << std::endl;
-                //TODO: create levels based on xml values
-                if(node.value() == "background") {
-                    std::cout << "greetings" << std::endl;
+                if(node.name() == platformName) {
+                    std::cout << "Greetings my dear sir" << std::endl;
                 }
+                for(pugi::xml_attribute attribute = node.first_attribute(); attribute; attribute = attribute.next_attribute()) {
+                    std::cout << "\t\t" << attribute.name() << ": " << attribute.value() << std::endl;
+                }
+                //TODO: create levels based on xml values
             }
         }
     } else {
